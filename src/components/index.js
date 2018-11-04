5d62f15e6738d125dd2457198583adab33f26de4
Shiau-uen Ding <ssding@yahoo.com>
2018-11-04 15:23:23 -0500

    cleanup

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export { default as Navbar } from './Navbar';
 export { default as Routes } from './Routes';
 export { default as Search } from './Search';
 export { default as Gifs } from './Gifs';
