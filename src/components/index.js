b6757112a94a30cd88feb75a727f121a2968d813
Shiau-uen Ding <ssding@yahoo.com>
2018-11-04 21:28:10 -0500

    App.test.js passed

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Routes } from './Routes';
 export { default as Search } from './Search';
 export { default as Gifs } from './Gifs';
 export { default as Favorites } from './Favorites';
+export { default as App } from './App';
