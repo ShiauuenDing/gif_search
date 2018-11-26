20702882f77bddde73d9c1928a2b787d7bd3e550
Shiau-uen Ding <ssding@yahoo.com>
2018-11-26 09:38:00 -0500

    navbar space fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,22 +3,22 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 function Navbar () {
-	return (
+  return (
     <nav>
-    	<div className='navWide'>
-    		<div className='wideDiv'>
-		      <Link to='/search'>new search</Link> | 
-		      <Link to='/favorites'>favorites</Link>
-		    </div>
-		  </div>
-		  <div className='navNarrow'>
-		  	<div className='narrowLinks'>
-		  		<Link to='/search'>new search</Link> | 
-		      <Link to='/favorites'>favorites</Link>
-		    </div>
-		  </div>
+      <div className='navWide'>
+        <div className='wideDiv'>
+          <Link to='/search'>new search</Link> | 
+          <Link to='/favorites'>favorites</Link>
+        </div>
+      </div>
+      <div className='navNarrow'>
+        <div className='narrowLinks'>
+          <Link to='/search'>new search</Link> | 
+          <Link to='/favorites'>favorites</Link>
+        </div>
+      </div>
     </nav>
-	)
+  )
 }
 
 export default Navbar;
